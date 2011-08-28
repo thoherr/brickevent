3afea34bc20a0450bbc946e6efb0036440180892
Thomas Herrmann <Thomas.Herrmann@teleteach.de>
2011-08-28 22:58:11 +0200

    devise dependencies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,8 @@ BrickEvent::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'https://brickevent.teleteach.de' }
+
   # Enable threaded mode
   # config.threadsafe!
 
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ BrickEvent::Application.routes.draw do
     resources :exhibits do as_routes end
   end
 
+  root :to => "events#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
