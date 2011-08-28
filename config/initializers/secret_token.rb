1add1e0c47c04dabebc425fb057b85c55d099fdd
Thomas Herrmann <Thomas.Herrmann@teleteach.de>
2011-08-28 14:05:13 +0200

    initial (empty rails 3.0.10 project)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrickEvent::Application.config.secret_token = 'b880afd36b6f382f10531bd4e0199c476f98f33b9bc8d94459e3f3efe50d0b65bbf2cb1a7dc74638ce61211858468a397dd70e8422b92dc2e9cc8d87b9cb2477'
