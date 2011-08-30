51874348214aabe64220e8a5d00587eabe0907a0
Thomas Herrmann <Thomas.Herrmann@teleteach.de>
2011-08-30 22:05:00 +0200

    change devise config to accept sign_out by :get method, because :delete does not work for some reason....

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 BrickEvent::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users do
+    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
 
   resources :users
   resources :events
