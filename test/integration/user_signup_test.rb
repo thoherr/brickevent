725f620d088b004dba9a68ee700b1f4708277eb1
Thomas Herrmann <Thomas.Herrmann@teleteach.de>
2012-01-22 23:40:49 +0100

    tests back to green....

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -43,7 +43,7 @@ class UserSignupTest < ActionDispatch::IntegrationTest
     assert page.has_content?('Bricking Bavaria e.V.'), "Bricking Bavaria e.V."
     assert page.has_content?('Veranstaltungsübersicht'), "Veranstaltungsübersicht"
     assert page.has_content?('Meine Anmeldungen'), "Meine Anmeldungen"
-    assert page.has_content?('ausloggen'), "ausloggen"
+    assert page.has_content?('Ausloggen'), "Ausloggen"
 
   end
 
@@ -58,7 +58,7 @@ class UserSignupTest < ActionDispatch::IntegrationTest
     click_on 'submit'
 
     assert page.has_content?('Meine Anmeldungen'), "Meine Anmeldungen"
-    assert page.has_content?('ausloggen'), "ausloggen"
+    assert page.has_content?('Ausloggen'), "Ausloggen"
 
   end
 
@@ -66,7 +66,7 @@ class UserSignupTest < ActionDispatch::IntegrationTest
 
     login_dummy_user
     assert page.has_content?('Meine Anmeldungen'), "Meine Anmeldungen"
-    assert page.has_content?('ausloggen'), "ausloggen"
+    assert page.has_content?('Ausloggen'), "Ausloggen"
 
     visit new_attendance_path(:event => events(:one).id, :user_id => @user.id)
     assert page.has_content?('Anmeldung für'), "Anmeldung für"
@@ -76,7 +76,7 @@ class UserSignupTest < ActionDispatch::IntegrationTest
     assert page.has_content?('Anmeldung für'), "Anmeldung für"
     # TODO: This should be true, but it isn't.....
     # CHECK
-#    assert page.has_content?('Du hast bisher 1 Person angemeldet'), "Du hast bisher 1 Person angemeldet"
+    #assert page.has_content?('Du hast bisher 1 Person angemeldet'), "Du hast bisher 1 Person angemeldet"
 
   end
 
