b857189395da7638fda4558f8a40339281ec395f
Thomas Herrmann <Thomas.Herrmann@teleteach.de>
2012-02-10 20:09:43 +0100

    texts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,7 @@ BrickEvent::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'bb2012.de' }
   config.action_mailer.delivery_method = :sendmail
+  
   # Defaults to:
   # config.action_mailer.sendmail_settings = {
   #   :location => '/usr/sbin/sendmail',
