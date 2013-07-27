a9b123b341fcf1d7672aa4bdb43a111eed536249
Thomas Herrmann <Thomas.Herrmann@teleteach.de>
2013-07-27 12:40:49 +0200

    add config/setup_load_paths.rb for passenger

---
+++ config/setup_load_paths.rb
@@ -0,0 +1,20 @@
+# this is to ensure that passenger on the web server uses the correct (i.e. project-specific) gem set
+# code from http://everydayrails.com/2010/09/13/rvm-project-gemsets.html
+
+# config/setup_load_paths.rb
+
+if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
+  begin
+    rvm_path = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
+    rvm_lib_path = File.join(rvm_path, 'lib')
+    LOAD_PATH.unshift rvm_lib_path
+    require 'rvm'
+    RVM.use_from_path! File.dirname(File.dirname(__FILE__))
+  rescue LoadError
+    # RVM is unavailable at this point.
+    raise "RVM ruby lib is currently unavailable."
+  end
+end
+
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
+require 'bundler/setup'
