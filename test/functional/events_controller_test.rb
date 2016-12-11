bceb1c3db61a9847ab8f41d18fda9c2d57ffd154
Thomas Herrmann <mail@thoherr.de>
2016-12-12 00:10:47 +0100

    get rid of unused controllers and views; I18n

--- test/functional/events_controller_test.rb
+++ test/functional/events_controller_test.rb
@@ -16,19 +16,6 @@ class EventsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:events)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create event" do
-    assert_difference('Event.count') do
-      post :create, :event => @event.attributes
-    end
-
-    assert_redirected_to event_path(assigns(:event))
-  end
-
   test "should show event" do
     get :show, :id => @event.to_param
     assert_response :success
@@ -44,23 +31,5 @@ class EventsControllerTest < ActionController::TestCase
     get :exhibits_as_csv, :id => events(:three).to_param
     assert_response :success
     assert_equal "Bestätigt;Name;MOC;Beschreibung;URL;Größe in Studs;Größe;Größe x;Größe y;Größe z;Größe Einheit;Größe x (m);Größe y (m);Größe z (m);Versicherungswert;Baustunden;Anzahl Steine;Strom?;Sammeltransport;Gemeinschaftsprojekt?;Teil Gemeinschaftsprojekt;Name Gemeinschaftsprojekt\nfalse;MyString;Another great MOC;Even more awesome;MyString;2400 * 6400;Huge;24;64;;cm;24.0;64.0;;10;10.0;1;false;true;false;false;-\ntrue;MyString;My first extraordinary MOC;Very awesome;MyString;160 by 240;MyString;1;2;;cm;1.6;2.4;;1000;1;1;false;false;false;false;-\n", response.body
-  end
-
-  test "should get edit" do
-    get :edit, :id => @event.to_param
-    assert_response :success
-  end
-
-  test "should update event" do
-    put :update, :id => @event.to_param, :event => @event.attributes
-    assert_redirected_to event_path(assigns(:event))
-  end
-
-  test "should destroy event" do
-    assert_difference('Event.count', -1) do
-      delete :destroy, :id => @event.to_param
-    end
-
-    assert_redirected_to events_path
   end
 end
