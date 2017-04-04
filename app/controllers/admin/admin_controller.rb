d493edc196c916a4f074a04cbe2e2b365ff5a172
Thomas Herrmann <mail@thoherr.de>
2017-04-05 01:30:23 +0200

    secure admin pages; add missing translation for admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,10 +3,16 @@ module Admin
     layout "admin"
 
     before_filter :authenticate_user!
+    before_filter :redirect_non_admins_to_frontend!
 
     ActiveScaffold.set_defaults do |config| 
       config.ignore_columns.add [:created_at, :updated_at, :lock_version]
     end
 
+    def redirect_non_admins_to_frontend!
+      unless current_user.is_admin?
+        redirect_to events_url
+      end
+    end
   end
 end
