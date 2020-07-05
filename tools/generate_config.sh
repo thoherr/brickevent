73c3147793ad8924b902a3cd2964688341f4dab7
Thomas Herrmann <mail@thoherr.de>
2020-07-05 19:24:44 +0200

    add docker config templates and secrets setup

--- tools/generate_config.sh
+++ tools/generate_config.sh
@@ -17,12 +17,21 @@ fi
 
 DB_PASSWORD=$(pwgen 16 1)
 DB_ROOT_PASSWORD=$(pwgen 16 1)
+SECRET_KEY=$(pwgen 64 1)
+SECRET_TOKEN=$(pwgen 64 1)
 
-echo Generating files from teamplates with generated password
+echo Generating files from templates with generated passwords and secret keys
 sed -e "s/DB_PASSWORD_PLACEHOLDER/$DB_PASSWORD/g" -e "s/DB_ROOT_PASSWORD_PLACEHOLDER/$DB_ROOT_PASSWORD/g" \
     < docker-compose.yml.template \
     > docker-compose.yml
 sed -e "s/DB_PASSWORD_PLACEHOLDER/$DB_PASSWORD/g" -e "s/DB_ROOT_PASSWORD_PLACEHOLDER/$DB_ROOT_PASSWORD/g" \
     < mysql/my.cnf.template \
     > mysql/my.cnf
+sed -e "s/SECRET_KEY_PLACEHOLDER/$SECRET_KEY/g" \
+    < config/devise.rb.template \
+    > config/devise.rb
+sed -e "s/SECRET_TOKEN_PLACEHOLDER/$SECRET_TOKEN/g" \
+    < config/secret_token.rb.template \
+    > config/secret_token.rb
+cp config/production.rb.template config/production.rb
 
