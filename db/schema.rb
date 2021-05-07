175e86228ac9c480a15594c637fe593b3cf45124
Thomas Herrmann <mail@thoherr.de>
2021-05-07 14:20:33 +0200

    use cm as primary unit for conversion and export

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_122409) do
+ActiveRecord::Schema.define(version: 2021_05_07_114431) do
 
   create_table "accommodation_types", force: :cascade do |t|
     t.string "name"
@@ -167,7 +167,7 @@ ActiveRecord::Schema.define(version: 2021_05_06_122409) do
   create_table "units", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.decimal "factor", precision: 7, scale: 5
+    t.decimal "centimeter", precision: 7, scale: 5
     t.datetime "created_at"
     t.datetime "updated_at"
   end
