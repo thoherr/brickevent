027e486ab5163b1ab3272b8bc531e57c046047fb
Thomas Herrmann <mail@thoherr.de>
2021-05-07 17:22:45 +0200

    number of shirts must be positive if size given (#51)

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -5,30 +5,30 @@ class Attendee < ApplicationRecord
   validates_presence_of :attendance
   validates_presence_of :attendee_type
 
+  validate :shirt_count_positive_when_size_given
+
   def event
-    return attendance.event if attendance
-    return nil
+    attendance&.event
   end
 
   def event_title
-    return attendance.event_title if attendance
-    return "NO ATTENDANCE"
+    attendance&.event_title || "NO ATTENDANCE"
   end
 
   def attendee_email
     return self.email unless self.email.blank?
-    return attendance.user.email unless attendance.blank? or attendance.user.blank?
-    return ""
+    return attendance&.user&.email unless attendance&.user.blank?
+    ""
   end
 
   def phone
-    return attendance.user.phone unless attendance.blank? or attendance.user.blank?
-    return ""
+    return attendance&.user&.phone unless attendance&.user.blank?
+    ""
   end
 
   def address
-    return attendance.user.address unless attendance.blank? or attendance.user.blank?
-    return ""
+    return attendance&.user&.address unless attendance&.user.blank?
+    ""
   end
 
   def to_s
@@ -39,18 +39,25 @@ class Attendee < ApplicationRecord
   def Attendee.csv_array_header(event)
        return ["ID","Typ","Bestätigt","Name","LUG","Nickname","EMail","Telefon", "Adresse", "AFOLs-Abend","Ticket",event.label_option_1,event.label_option_2,event.label_option_3,event.label_option_4,event.label_option_5,"Bemerkungen","Anzahl Event-Shirts","Shirt-Größe","Zuletzt geändert"]
   end
+
   def csv_array
-    return [ id,
-             attendee_type.name, is_approved?,
-             StringSanitizer.sanitize_encoding(name),
-             StringSanitizer.sanitize_encoding(lug),
-             StringSanitizer.sanitize_encoding(nickname),
-             email, phone,
-             StringSanitizer.sanitize_encoding(address),
-             afols_event, needs_ticket, option_1, option_2, option_3, option_4, option_5,
-             StringSanitizer.sanitize_encoding(remarks),
-             number_of_shirts, shirt_size,
-             updated_at.strftime("%F %T") ]
+    [id,
+     attendee_type.name, is_approved?,
+     StringSanitizer.sanitize_encoding(name),
+     StringSanitizer.sanitize_encoding(lug),
+     StringSanitizer.sanitize_encoding(nickname),
+     email, phone,
+     StringSanitizer.sanitize_encoding(address),
+     afols_event, needs_ticket, option_1, option_2, option_3, option_4, option_5,
+     StringSanitizer.sanitize_encoding(remarks),
+     number_of_shirts, shirt_size,
+     updated_at.strftime("%F %T")]
+  end
+
+  protected
+
+  def shirt_count_positive_when_size_given
+    errors.add('T-Shirts', 'Die Anzahl muss größer 0 sein, wenn eine Größe ausgewählt wurde') if number_of_shirts.blank? && !shirt_size.blank?
   end
 
 end
