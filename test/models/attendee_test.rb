0b1a1d12a3ce864ea6c8ddffa5b315937fe49f0e
Thomas Herrmann <mail@thoherr.de>
2021-05-07 17:09:14 +0200

    number of shirts must be positive if size given

--- test/models/attendee_test.rb
+++ test/models/attendee_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class AttendeeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "number of shirts ok" do
+    has_shirt_count = attendees(:two)
+    assert has_shirt_count.valid?
+    assert has_shirt_count.errors.empty?
+    expected_messages = {}
+    assert_equal expected_messages, has_shirt_count.errors.messages
+    missing_shirt_count = attendees(:four)
+    assert_not missing_shirt_count.valid?
+    assert_not missing_shirt_count.errors.empty?
+    expected_messages = {:"T-Shirts"=>["Die Anzahl muss größer 0 sein, wenn eine Größe ausgewählt wurde"]}
+    assert_equal expected_messages, missing_shirt_count.errors.messages
+  end
 end
