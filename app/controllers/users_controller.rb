d98f813532fd14197f9e07b7c9dcb99b38747fc2
Thomas Herrmann <mail@thoherr.de>
2023-12-23 17:03:39 +0100

    avoid editing foreign user data (except for admins)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,13 @@
 class UsersController < ApplicationController
   # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
+    get_user
   end
 
   # PUT /users/1
   # PUT /users/1.json
   def update
-    @user = User.find(params[:id])
+    get_user
 
     respond_to do |format|
       if @user.update(user_params)
@@ -22,6 +22,14 @@ class UsersController < ApplicationController
   end
 
   private
+  def get_user
+    id = params[:id].to_i
+    unless current_user.id == id || current_user.is_admin?
+      raise 'unauthorized request'
+    end
+    @user = User.find(id)
+  end
+
   def user_params
     # FIXME: This is just a temporary wild card to get the app running on Raila 4.0
     params.require(:user).permit!
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -2,18 +2,49 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   setup do
-    @user = users(:thoherr)
+    @user = users(:one)
     @user.confirm
     sign_in @user
   end
 
-  test "should get edit" do
+  test "user should get edit for itself" do
     get :edit, params: { id: @user.to_param }
     assert_response :success
   end
 
-  test "should update user" do
+  test "user should get edit when admin" do
+    @user = users(:thoherr)
+    @user.confirm
+    sign_in @user
+
+    get :edit, params: { id: users(:two).to_param }
+    assert_response :success
+  end
+
+  test "should get unauthorized access when editing another user" do
+    assert_raise do
+      get :edit, params: { id: users(:two).to_param }
+    end
+  end
+
+  test "should update own data" do
     put :update, params: { id: @user.to_param, user: @user.attributes }
     assert_redirected_to events_path
   end
+
+  test "should get unauthorized access when updating another user" do
+    assert_raise do
+      put :update, params: { id: users(:two).to_param, user: users(:two).attributes }
+    end
+  end
+
+  test "should update data of another user when admin" do
+    @user = users(:thoherr)
+    @user.confirm
+    sign_in @user
+
+    put :update, params: { id: users(:one).to_param, user: users(:one).attributes }
+    assert_redirected_to events_path
+  end
+
 end
