2be0c3d300c766d631304093a579a90e505fc53a
Thomas Herrmann <mail@thoherr.de>
2023-12-23 20:02:57 +0100

    protect exhibits from foreign access

--- test/functional/exhibits_controller_test.rb
+++ test/functional/exhibits_controller_test.rb
@@ -2,11 +2,10 @@ require 'test_helper'
 
 class ExhibitsControllerTest < ActionController::TestCase
   setup do
-    user = User.first
+    user = users(:one)
     user.confirm
     sign_in user
     @exhibit = exhibits(:one)
-    @exhibit.attendance = attendances(:one)
     @exhibit.unit = units(:studs)
   end
 
@@ -28,7 +27,41 @@ class ExhibitsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should not get edit if not owner" do
+    user = users(:three)
+    user.confirm
+    sign_in user
+    assert_raise do
+      get :edit, params: { id: @exhibit.to_param }
+    end
+  end
+
+  test "should get edit if admin" do
+    user = users(:thoherr)
+    user.confirm
+    sign_in user
+    get :edit, params: { id: @exhibit.to_param }
+    assert_response :success
+  end
+
   test "should update exhibit" do
+    put :update, params: { id: @exhibit.to_param, exhibit: @exhibit.attributes }
+    assert_redirected_to attendance_path(assigns(:exhibit).attendance)
+  end
+
+  test "should not update exhibit if unauthorized" do
+    user = users(:three)
+    user.confirm
+    sign_in user
+    assert_raise do
+      put :update, params: { id: @exhibit.to_param, exhibit: @exhibit.attributes }
+    end
+  end
+
+  test "should update exhibit if event manager" do
+    user = users(:two)
+    user.confirm
+    sign_in user
     put :update, params: { id: @exhibit.to_param, exhibit: @exhibit.attributes }
     assert_redirected_to attendance_path(assigns(:exhibit).attendance)
   end
@@ -40,4 +73,36 @@ class ExhibitsControllerTest < ActionController::TestCase
 
     assert_redirected_to attendance_path attendances(:one)
   end
+
+  test "should not destroy exhibit if unauthorized" do
+    user = users(:three)
+    user.confirm
+    sign_in user
+    assert_raise do
+      delete :destroy, params: { id: @exhibit.to_param }
+    end
+  end
+
+  test "should not be approvable by user" do
+    assert_raise do
+      post :approve, params: { id: @exhibit.id }
+    end
+  end
+
+  test "should be approvable by event manager" do
+    @user = users(:thoherr)
+    @user.confirm
+    sign_in @user
+    post :approve, params: { id: @exhibit.id }
+    assert_redirected_to event_path(@exhibit.attendance.event)
+  end
+
+  test "should be approvable by admin" do
+    @user = users(:two)
+    @user.confirm
+    sign_in @user
+    post :approve, params: { id: @exhibit.id }
+    assert_redirected_to event_path(@exhibit.attendance.event)
+  end
+
 end
