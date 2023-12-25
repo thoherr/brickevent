4eafbf950aeadecc1b93ada3f7d594907cc0cfa2
Thomas Herrmann <mail@thoherr.de>
2023-12-25 22:42:56 +0100

    get rid of comment lines as preperation to replace active_scaffold

--- app/controllers/admin/attendances_controller.rb
+++ app/controllers/admin/attendances_controller.rb
@@ -3,13 +3,9 @@ module Admin
     active_scaffold :attendance do |config|
       list_columns = [:user, :event, :is_approved]
       show_columns = list_columns
-#      update_columns = [:user_type, :email, :firstname, :lastname, :title, :salutation, :street_1, :street_2, :zipcode, :city, :tel, :institution, :sign_in_count, :tnc, :pp]
-#      create_columns = update_columns
 
      config.list.columns = list_columns
       config.show.columns = show_columns
-#      config.update.columns = update_columns
-#      config.create.columns = create_columns
     end
   end
 end
