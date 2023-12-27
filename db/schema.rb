81b043e435e27d3654d0ff261eb2ad4e41bd33d3
Thomas Herrmann <mail@thoherr.de>
2023-12-27 21:19:41 +0100

    upgrade to rails 7.0(.8)

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_25_225439) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_12_27_201255) do
   create_table "accommodation_types", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.integer "size"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "accommodations", force: :cascade do |t|
@@ -25,15 +24,15 @@ ActiveRecord::Schema.define(version: 2023_12_25_225439) do
     t.integer "accommodation_type_id"
     t.integer "count"
     t.text "remarks", limit: 65384
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "attendances", force: :cascade do |t|
     t.integer "user_id"
     t.integer "event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.boolean "is_approved"
     t.index ["user_id", "event_id"], name: "index_attendances_on_user_id_and_event_id", unique: true
   end
@@ -41,8 +40,8 @@ ActiveRecord::Schema.define(version: 2023_12_25_225439) do
   create_table "attendee_types", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.boolean "is_visible"
   end
 
@@ -55,8 +54,8 @@ ActiveRecord::Schema.define(version: 2023_12_25_225439) do
     t.string "email"
     t.boolean "afols_event"
     t.text "remarks", limit: 65384
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "shirt_size"
     t.boolean "is_approved"
     t.boolean "needs_ticket", default: true
@@ -71,15 +70,15 @@ ActiveRecord::Schema.define(version: 2023_12_25_225439) do
   create_table "builders", force: :cascade do |t|
     t.integer "exhibit_id"
     t.integer "attendee_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "event_managers", force: :cascade do |t|
     t.integer "event_id"
     t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "events", force: :cascade do |t|
@@ -93,8 +92,8 @@ ActiveRecord::Schema.define(version: 2023_12_25_225439) do
     t.date "start_date"
     t.date "end_date"
     t.text "remarks", limit: 65384
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.boolean "has_event_shirt"
     t.boolean "has_accommodation"
     t.boolean "has_moc_card_service"
@@ -135,8 +134,8 @@ ActiveRecord::Schema.define(version: 2023_12_25_225439) do
     t.boolean "is_part_of_installation"
     t.integer "installation_exhibit_id"
     t.text "remarks", limit: 65384
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "size_x"
     t.integer "size_y"
     t.integer "size_z"
@@ -158,8 +157,8 @@ ActiveRecord::Schema.define(version: 2023_12_25_225439) do
     t.string "logo_url"
     t.string "info_mail"
     t.string "request_pattern"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "impress_url"
     t.string "favicon_url"
   end
@@ -168,19 +167,19 @@ ActiveRecord::Schema.define(version: 2023_12_25_225439) do
     t.string "name"
     t.string "description"
     t.decimal "centimeter", precision: 7, scale: 4
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "encrypted_password", limit: 128, default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "name"
@@ -189,11 +188,11 @@ ActiveRecord::Schema.define(version: 2023_12_25_225439) do
     t.string "address"
     t.string "phone"
     t.boolean "is_admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
     t.boolean "accept_data_storage"
     t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
