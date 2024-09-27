d278103592f046cac32c3c7ee761d57441a053cd
Thomas Herrmann <mail@thoherr.de>
2024-09-27 16:37:41 +0200

    upgate schema.rb to 7.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_27_201255) do
+ActiveRecord::Schema[7.2].define(version: 2023_12_27_201255) do
   create_table "accommodation_types", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -199,5 +199,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_27_201255) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
