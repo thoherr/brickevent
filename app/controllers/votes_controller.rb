e286f59ba7e00591e1f868b7735f248a85b13e45
Thomas Herrmann <mail@thoherr.de>
2024-10-06 12:23:16 +0200

    add voting test, cleanup code

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,11 +6,8 @@ class VotesController < ApplicationController
   # GET /exhibits/:id/votes/new
   # GET /exhibits/:id/votes/new.json
   def new
-    load_exhibit_and_event
-    if @exhibit
-      visitor = Visitor.load_or_create(session[:session_id])
-      @voted_already = visitor.voted_for? @exhibit, vote_scope: @exhibit.current_voting_scope
-    end
+    load_visitor_exhibit_and_event
+    @voted_already = @visitor.voted_for? @exhibit, vote_scope: @exhibit.current_voting_scope
     @vote = Vote.new
     render :layout => 'voting'
   end
@@ -18,33 +15,32 @@ class VotesController < ApplicationController
   # POST /exhibits/:exhibit_id/votes
   # POST /exhibits/:exhibit_id/votes.json
   def create
-    load_exhibit_and_event
-    if @exhibit
-      visitor = Visitor.load_or_create(session[:session_id])
-      @voted_already = visitor.voted_for? @exhibit, vote_scope: @exhibit.current_voting_scope
-      if @voted_already
-        redirect_to new_vote_path(@exhibit)
-      end
-      if visitor
-        @exhibit.liked_by visitor, vote_scope: @exhibit.current_voting_scope
-        respond_to do |format|
-          if @exhibit.save
-            format.html { redirect_to new_vote_path(@exhibit) }
-            format.json { render :json => @exhibit.to_s, :status => :updated, :location => @event }
-          else
-            format.html { redirect_to new_vote_path(@exhibit) }
-            format.json { render :json => @exhibit.errors, :status => :unprocessable_entity }
-          end
+    load_visitor_exhibit_and_event
+    @voted_already = @visitor.voted_for? @exhibit, vote_scope: @exhibit.current_voting_scope
+    if @voted_already
+      redirect_to new_vote_path(@exhibit, :notice => t('cannot_vote_twice'))
+    else
+      @exhibit.liked_by @visitor, vote_scope: @exhibit.current_voting_scope
+      respond_to do |format|
+        if @exhibit.save
+          format.html { redirect_to new_vote_path(@exhibit) }
+          format.json { render :json => @exhibit.to_s, :status => :updated, :location => @event }
+        else
+          format.html { redirect_to new_vote_path(@exhibit) }
+          format.json { render :json => @exhibit.errors, :status => :unprocessable_entity }
         end
       end
     end
   end
 
-private
+  private
 
-  def load_exhibit_and_event
+  def load_visitor_exhibit_and_event
+    @visitor = Visitor.load_or_create(session[:session_id])
     @exhibit = Exhibit.find(params[:id])
+    raise 'INVALID REQUEST' unless @exhibit and @visitor
     @event = @exhibit.attendance.event
+    raise 'INVALID REQUEST' unless @event
   end
 
 end
--- test/functional/votes_controller_test.rb
+++ test/functional/votes_controller_test.rb
@@ -11,8 +11,57 @@ class VotesControllerTest < ActionController::TestCase
   end
 
   test "should create vote" do
-    post :create, params: { id: @exhibit.id }
-    assert_redirected_to new_vote_path(assigns(:exhibit))
+    request.session[:session_id] = 42
+    assert_difference 'Vote.count' do
+      post :create, params: { id: @exhibit.id }
+      assert_response :redirect
+      assert_redirected_to new_vote_path(assigns(:exhibit))
+    end
+    request.session[:session_id] = 48
+    assert_difference 'Vote.count' do
+      post :create, params: { id: @exhibit.id }
+      assert_response :redirect
+      assert_redirected_to new_vote_path(assigns(:exhibit))
+    end
   end
 
+  test "cannot vote twice" do
+    request.session[:session_id] = 42
+    assert_difference 'Vote.count' do
+      post :create, params: { id: @exhibit.id }
+    end
+    assert_difference 'Vote.count', 0 do
+      post :create, params: { id: @exhibit.id }
+      assert_redirected_to new_vote_path(assigns(:exhibit), :notice => I18n.t('cannot_vote_twice'))
+    end
+
+    request.session[:session_id] = 48
+    assert_difference 'Vote.count' do
+      post :create, params: { id: @exhibit.id }
+    end
+    assert_difference 'Vote.count', 0 do
+      post :create, params: { id: @exhibit.id }
+      assert_redirected_to new_vote_path(assigns(:exhibit), :notice => I18n.t('cannot_vote_twice'))
+    end
+
+    request.session[:session_id] = 42
+    assert_difference 'Vote.count', 0 do
+      post :create, params: { id: @exhibit.id }
+      assert_redirected_to new_vote_path(assigns(:exhibit), :notice => I18n.t('cannot_vote_twice'))
+    end
+
+    request.session[:session_id] = 44
+    assert_difference 'Vote.count' do
+      post :create, params: { id: @exhibit.id }
+      assert_response :redirect
+      assert_redirected_to new_vote_path(assigns(:exhibit))
+    end
+  end
+
+  test "wrong data should raise error" do
+    request.session[:session_id] = 42
+    assert_raise ActiveRecord::RecordNotFound do
+      post :create, params: { id: 12345 }
+    end
+  end
 end
