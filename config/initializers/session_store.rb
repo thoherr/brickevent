b0e87185222f1727a6aae95a9174217dec29fd6d
Thomas Herrmann <mail@thoherr.de>
2024-10-06 22:00:48 +0200

    set explicit session expiration to avoid session to be cleared when browser closes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_brick_event_session'
+# explicit expiration avoids session to be cleared on browser closing, see
+# https://stackoverflow.com/a/77865002
+Rails.application.config.session_store :cookie_store, expire_after: 14.days
