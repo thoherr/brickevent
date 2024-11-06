592c676ca1702fde6f66d664b084dbbce69304d8
Thomas Herrmann <mail@thoherr.de>
2024-11-06 21:59:11 +0100

    asset pipeline for (s)css and js

---
+++ app/javascript/application.js
@@ -0,0 +1,5 @@
+// Entry point for the build script in your package.json
+
+//= require jquery
+//= require jquery_ujs
+//= require active_scaffold
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path    = require("path")
+const webpack = require("webpack")
+
+module.exports = {
+  mode: "production",
+  devtool: "source-map",
+  entry: {
+    application: "./app/javascript/application.js"
+  },
+  resolve: {
+    modules: ["app/javascript", "node_modules"],
+  },
+  output: {
+    filename: "[name].js",
+    sourceMapFilename: "[file].map",
+    chunkFormat: "module",
+    path: path.resolve(__dirname, "app/assets/builds"),
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  ]
+}
